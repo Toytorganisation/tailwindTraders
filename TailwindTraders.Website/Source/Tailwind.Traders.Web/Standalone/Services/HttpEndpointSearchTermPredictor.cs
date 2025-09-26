a9972f8a398a28872a99bcd55fc959278f4dfb37
yvanoumbe11 <oumbe.yvan@yahoo.com>
2025-09-26 10:26:17 +0200

    Update HttpEndpointSearchTermPredictor.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
@@ -1,20 +1,19 @@
-using System.IO;
-using System.Threading.Tasks;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.Processing;
-using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
-using Microsoft.Extensions.Logging;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Specialized;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace Tailwind.Traders.Web.Standalone.Services
 {
-   TermPredictor : IImageSearchTermPredictor
+    public class HttpEndpointSearchTermPredictor : IImageSearchTermPredictor
     {
         private readonly ILogger<HttpEndpointSearchTermPredictor> logger;
         private readonly string imageEndpoint;
@@ -24,6 +23,7 @@ namespace Tailwind.Traders.Web.Standalone.Services
         {
             this.logger = logger;
             this.imageEndpoint = config["ImagePredictorEndpoint"];
+
             try
             {
                 blobServiceClient = new BlobServiceClient(config["StorageConnectionString"]);
@@ -36,59 +36,72 @@ namespace Tailwind.Traders.Web.Standalone.Services
 
         async Task<string> IImageSearchTermPredictor.PredictSearchTerm(Stream imageStream)
         {
-           Async(imageStream);
-            var image = imageWithFormat.Image;
-            var imageFormat = imageWithFormat.Format;
+            using (var image = Image.Load<Rgba32>(imageStream, out var format))
+            {
+                // resize image constraining it to 500px in any dimension
+                var resizedImage = image.Clone(
+                    ctx => ctx.Resize(new ResizeOptions
+                    {
+                        Size = new SixLabors.ImageSharp.Size(500, 500),
+                        Mode = ResizeMode.Max
+                    }));
 
-            // resize image constraining it to 500px in any dimension
-            var resizedImage = image.Clone(
-                ctx => ctx.Resize(new ResizeOptions {
-                    Size = new SixLabors.ImageSharp.Size(500, 500),
-                    Mode = ResizeMode.Max
-                }));
-            
-            // upload the file
-            var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
-            var filename = Guid.NewGuid().ToString() + ".jpg";
-            var cloudBlockBlob = blobContainerClient.GetBlockBlobClient(filename);
-            var blobStream = new MemoryStream();
-            resizedImage.SaveAsJpeg(blobStream);
-            blobStream.Seek(0, SeekOrigin.Begin);
-            await cloudBlockBlob.UploadAsync(blobStream);
+                // upload the file
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
+                var filename = Guid.NewGuid().ToString() + ".jpg";
+                var cloudBlockBlob = blobContainerClient.GetBlobClient(filename);
 
-            logger.LogInformation(eventId, "Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
+                using (var blobStream = new MemoryStream())
+                {
+                    resizedImage.SaveAsJpeg(blobStream);
+                    blobStream.Seek(0, SeekOrigin.Begin);
+                    await cloudBlockBlob.UploadAsync(blobStream);
+                }
 
-            // pass the file to the endpoint
-            var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync(fullEndpoint);
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStreamAsync();
+                logger.LogInformation("Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
 
-            var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
-            var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
+                // pass the file to the endpoint
+                var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(fullEndpoint);
+                response.EnsureSuccessStatusCode();
 
-            logger.LogInformation(eventId, "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}", 
-                resultObject.prediction, resultObject.scores.hammer, resultObject.scores.wrench);
+                var responseBody = await response.Content.ReadAsStreamAsync();
 
-            return await Task.FromResult(resultObject.prediction);
+                var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
+                var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
+
+                logger.LogInformation(
+                    "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}",
+                    resultObject.prediction,
+                    resultObject.scores.hammer,
+                    resultObject.scores.wrench
+                );
+
+                return resultObject.prediction;
+            }
         }
     }
 
     [DataContract]
-    public class ImageRecognitionResult {
+    public class ImageRecognitionResult
+    {
         [DataMember]
         public string time { get; set; }
+
         [DataMember]
         public string prediction { get; set; }
+
         [DataMember]
         public PredictionScores scores { get; set; }
     }
 
     [DataContract]
-    public class PredictionScores {
+    public class PredictionScores
+    {
         [DataMember]
         public decimal hammer { get; set; }
+
         [DataMember]
         public decimal wrench { get; set; }
     }
