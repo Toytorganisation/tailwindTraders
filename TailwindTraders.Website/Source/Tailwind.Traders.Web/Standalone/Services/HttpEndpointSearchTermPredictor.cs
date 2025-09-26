ffd6bd4cc337ad2cb189f7530647e094bc791a70
yvanoumbe11 <oumbe.yvan@yahoo.com>
2025-09-26 11:33:37 +0200

    Update HttpEndpointSearchTermPredictor.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Net.Http;
@@ -10,7 +11,6 @@ using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
 using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Specialized;
 
 namespace Tailwind.Traders.Web.Standalone.Services
 {
@@ -38,45 +38,48 @@ namespace Tailwind.Traders.Web.Standalone.Services
         {
             var eventId = new EventId();
             IImageFormat imageFormat;
-            var image = Image.Load(imageStream, out imageFormat);
 
-            // resize image constraining it to 500px in any dimension
-            var resizedImage = image.Clone(
-                ctx => ctx.Resize(new ResizeOptions {
-                    Size = new Size(500,500),
-                    Mode = ResizeMode.Max
-                }));
-            
-            // upload the file
-            var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
-            var filename = Guid.NewGuid().ToString() + ".jpg";
-            var cloudBlockBlob = blobContainerClient.GetBlockBlobClient(filename);
-            var blobStream = new MemoryStream();
-            resizedImage.SaveAsJpeg(blobStream);
-            blobStream.Seek(0,SeekOrigin.Begin);
-            await cloudBlockBlob.UploadAsync(blobStream);
+            using (var image = Image.Load<Rgba32>(imageStream, out imageFormat))
+            {
+                // resize image constraining it to 500px in any dimension
+                var resizedImage = image.Clone(
+                    ctx => ctx.Resize(new ResizeOptions {
+                        Size = new Size(500, 500),
+                        Mode = ResizeMode.Max
+                    }));
 
-            logger.LogInformation(eventId, "Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
+                // upload the file
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
+                var filename = Guid.NewGuid().ToString() + ".jpg";
+                var cloudBlockBlob = blobContainerClient.GetBlockBlobClient(filename);
+                var blobStream = new MemoryStream();
+                resizedImage.SaveAsJpeg(blobStream);
+                blobStream.Seek(0, SeekOrigin.Begin);
+                await cloudBlockBlob.UploadAsync(blobStream);
 
-            // pass the file to the endpoint
-            var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync(fullEndpoint);
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStreamAsync();
+                logger.LogInformation(eventId, "Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
 
-            var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
-            var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
+                // pass the file to the endpoint
+                var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(fullEndpoint);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStreamAsync();
 
-            logger.LogInformation(eventId, "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}", 
-                resultObject.prediction, resultObject.scores.hammer, resultObject.scores.wrench);
+                var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
+                var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
 
-            return await Task.FromResult(resultObject.prediction);;
+                logger.LogInformation(eventId, "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}",
+                    resultObject.prediction, resultObject.scores.hammer, resultObject.scores.wrench);
+
+                return resultObject.prediction;
+            }
         }
     }
 
     [DataContract]
-    public class ImageRecognitionResult {
+    public class ImageRecognitionResult
+    {
         [DataMember]
         public string time { get; set; }
         [DataMember]
@@ -86,7 +89,8 @@ namespace Tailwind.Traders.Web.Standalone.Services
     }
 
     [DataContract]
-    public class PredictionScores {
+    public class PredictionScores
+    {
         [DataMember]
         public decimal hammer { get; set; }
         [DataMember]
