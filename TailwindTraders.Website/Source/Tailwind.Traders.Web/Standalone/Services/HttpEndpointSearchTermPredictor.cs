aab5e524cc99bd16e92e00a0f3c94bf04de35865
yvanoumbe11 <oumbe.yvan@yahoo.com>
2025-09-26 10:34:01 +0200

    Update HttpEndpointSearchTermPredictor.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/HttpEndpointSearchTermPredictor.cs
@@ -1,15 +1,17 @@
-using System;
 using System.IO;
-using System.Net.Http;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Processing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using SixLabors.Primitives;
 
 namespace Tailwind.Traders.Web.Standalone.Services
 {
@@ -23,7 +25,6 @@ namespace Tailwind.Traders.Web.Standalone.Services
         {
             this.logger = logger;
             this.imageEndpoint = config["ImagePredictorEndpoint"];
-
             try
             {
                 blobServiceClient = new BlobServiceClient(config["StorageConnectionString"]);
@@ -36,72 +37,59 @@ namespace Tailwind.Traders.Web.Standalone.Services
 
         async Task<string> IImageSearchTermPredictor.PredictSearchTerm(Stream imageStream)
         {
-            using (var image = Image.Load<Rgba32>(imageStream, out var format))
-            {
-                // resize image constraining it to 500px in any dimension
-                var resizedImage = image.Clone(
-                    ctx => ctx.Resize(new ResizeOptions
-                    {
-                        Size = new SixLabors.ImageSharp.Size(500, 500),
-                        Mode = ResizeMode.Max
-                    }));
+            var eventId = new EventId();
+            IImageFormat imageFormat;
+            var image = Image.Load(imageStream, out imageFormat);
 
-                // upload the file
-                var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
-                var filename = Guid.NewGuid().ToString() + ".jpg";
-                var cloudBlockBlob = blobContainerClient.GetBlobClient(filename);
+            // resize image constraining it to 500px in any dimension
+            var resizedImage = image.Clone(
+                ctx => ctx.Resize(new ResizeOptions {
+                    Size = new Size(500,500),
+                    Mode = ResizeMode.Max
+                }));
+            
+            // upload the file
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient("website-uploads");
+            var filename = Guid.NewGuid().ToString() + ".jpg";
+            var cloudBlockBlob = blobContainerClient.GetBlockBlobClient(filename);
+            var blobStream = new MemoryStream();
+            resizedImage.SaveAsJpeg(blobStream);
+            blobStream.Seek(0,SeekOrigin.Begin);
+            await cloudBlockBlob.UploadAsync(blobStream);
 
-                using (var blobStream = new MemoryStream())
-                {
-                    resizedImage.SaveAsJpeg(blobStream);
-                    blobStream.Seek(0, SeekOrigin.Begin);
-                    await cloudBlockBlob.UploadAsync(blobStream);
-                }
+            logger.LogInformation(eventId, "Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
 
-                logger.LogInformation("Image uploaded to {StorageUrl}", cloudBlockBlob.Uri.AbsoluteUri);
+            // pass the file to the endpoint
+            var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(fullEndpoint);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStreamAsync();
 
-                // pass the file to the endpoint
-                var fullEndpoint = imageEndpoint + cloudBlockBlob.Uri.AbsoluteUri;
-                HttpClient client = new HttpClient();
-                var response = await client.GetAsync(fullEndpoint);
-                response.EnsureSuccessStatusCode();
+            var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
+            var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
 
-                var responseBody = await response.Content.ReadAsStreamAsync();
+            logger.LogInformation(eventId, "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}", 
+                resultObject.prediction, resultObject.scores.hammer, resultObject.scores.wrench);
 
-                var serializer = new DataContractJsonSerializer(typeof(ImageRecognitionResult));
-                var resultObject = (ImageRecognitionResult)serializer.ReadObject(responseBody);
-
-                logger.LogInformation(
-                    "Result prediction: {prediction} with confidence - hammer: {hammerconf}, wrench: {wrenchconf}",
-                    resultObject.prediction,
-                    resultObject.scores.hammer,
-                    resultObject.scores.wrench
-                );
-
-                return resultObject.prediction;
-            }
+            return await Task.FromResult(resultObject.prediction);;
         }
     }
 
     [DataContract]
-    public class ImageRecognitionResult
-    {
+    public class ImageRecognitionResult {
         [DataMember]
         public string time { get; set; }
-
         [DataMember]
         public string prediction { get; set; }
-
         [DataMember]
         public PredictionScores scores { get; set; }
     }
 
     [DataContract]
-    public class PredictionScores
-    {
+    public class PredictionScores {
         [DataMember]
         public decimal hammer { get; set; }
-
         [DataMember]
         public decimal wrench { get; set; }
     }
